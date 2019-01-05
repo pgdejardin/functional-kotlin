2cb5237c3126b1e1e76429e3aa071f938c69c123
Paul-Guillaume Déjardin <pgdejardin@xebia.fr>
2019-01-05 20:25:50 +0100

    IO part done

---
+++ src/main/kotlin/fr/xebia/xke/fpkt/effects/IO.kt
@@ -0,0 +1,40 @@
+package fr.xebia.xke.fpkt.effects
+
+import arrow.core.Either
+import arrow.effects.IO
+import arrow.effects.instances.io.monad.binding
+import arrow.effects.liftIO
+
+/* IO is the most common data type used to represent side-effects in functional languages.
+   This means IO is the data type of choice when interacting with the external environment:
+   databases, network, operative systems, files…
+ */
+
+val ioMonadExample: IO<Unit> = IO { println("hello") }
+val ioMonadErrorExample: IO<Int> = IO { throw Exception("This a big error") }
+
+val getNumerator: IO<Int> = Math.random().toInt().liftIO()
+val getDenominator: IO<Int> = 0.liftIO()
+
+fun main() {
+  // Monad IO
+  ioMonadExample.attempt().unsafeRunSync()
+
+  ioMonadErrorExample.runAsync {
+    it.fold({ left -> IO { println(left.message) } }, { right -> IO { println(right.toString()) } })
+  }.unsafeRunSync()
+
+  val result = binding {
+    val numerator = getNumerator.bind()
+    val denominator = getDenominator.bind()
+
+    numerator / denominator
+  }
+    .attempt()
+    .unsafeRunSync()
+
+  when (result) {
+    is Either.Left -> println(result.a)
+    is Either.Right -> println(result.b)
+  }
+}
